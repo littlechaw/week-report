1017a23667cdd8b80cf1a8d96073404ffe257531
chaw-home <wangc2@ngarihealth.com>
2017-12-23 02:08:07 +0800

    something

---
+++ src/main/java/com/chaw/wong/repository/PersonRepository.java
@@ -0,0 +1,10 @@
+package com.chaw.wong.repository;
+
+import com.chaw.wong.entity.Person;
+
+/**
+ * Created by XRom
+ * On 11/16/2017.11:54 PM
+ */
+public interface PersonRepository extends DomainRepository<Person,Long>{
+}
---
+++ src/main/java/com/chaw/wong/service/PersonService.java
@@ -0,0 +1,9 @@
+package com.chaw.wong.service;
+
+/**
+ * Created by XRom
+ * On 11/16/2017.11:57 PM
+ */
+public interface PersonService {
+    Long savePerson();
+}
