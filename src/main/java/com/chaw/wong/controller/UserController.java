9b8e8b8358c4c295a68538f36ecc9eb6bc446693
xiaowc <wangc2@Ngarihealth.com>
2017-12-25 14:13:48 +0800

    wocao

--- src/main/java/com/chaw/wong/controller/UserController.java
+++ src/main/java/com/chaw/wong/controller/UserController.java
@@ -4,13 +4,23 @@ import com.chaw.wong.entity.User;
 import com.chaw.wong.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
+@RequestMapping(value = "/")
 public class UserController {
     @Autowired
     private UserService userService;
 
+    @RequestMapping(value = "login", method = RequestMethod.GET)
+    public ModelAndView toLogin(){
+        return new ModelAndView("login");
+    }
+
+    @RequestMapping(value = "user/getUserById", method = RequestMethod.GET)
     public User getUserById(String id) {
         return userService.getUserById(id);
     }
