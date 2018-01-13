7986074643e2d335aac5f27449ac99554fcf0975
wuk <wuk@ngarihealth.com>
2018-01-13 19:08:28 +0800

    modified

--- src/main/java/com/chaw/wong/oauth/gitlab/Application.java
+++ src/main/java/com/chaw/wong/oauth/gitlab/Application.java
@@ -1,14 +1,24 @@
 package com.chaw.wong.oauth.gitlab;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Application {
 
     private String url = "http://gitlab.ngarihealth.com";
     private String oauthUrl = url + "/oauth/authorize";
     private String tokenUrl = url + "/oauth/token";
     private String userUrl = url + "/api/v3/user?access_token=%s";
+    @Value("${gitlab.applicationId}")
     private String applicationId;
+    @Value("${gitlab.secret}")
     private String secret;
+    @Value("${gitlab.callbackUrl}")
     private String callbackUrl;
+
+    public Application() {
+    }
 
     public Application(String applicationId, String secret, String callbackUrl) {
         this.applicationId = applicationId;
